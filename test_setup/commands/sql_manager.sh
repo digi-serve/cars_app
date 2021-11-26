d3c7f05639a558db0781bb82d99b8ee4354566cf
nate <7259@pm.me>
2021-11-26 16:36:11 +0700

    [fix] folder name in sql manager

--- test_setup/commands/sql_manager.sh
+++ test_setup/commands/sql_manager.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 Runtime_Dirname=${PWD%/*/*}
 Module="cars"
+Folder=$1
 ID_Service=`docker ps | grep "test_" | grep "_db" | awk '{ print $1 }'`
 for FILE_SQL in "$@"; do
 	if [ -z "$ID_Service" ]
@@ -12,7 +13,7 @@ for FILE_SQL in "$@"; do
 		docker ps
 		echo ""
 	else
-		DB_Init=`cat ./cypress/integration/test_$Module/test_setup/sql/$FILE_SQL`
+		DB_Init=`cat ./cypress/integration/$Folder/test_setup/sql/$FILE_SQL`
 		docker exec $ID_Service bash -c "echo '$DB_Init' > ./sql/${Module}_$FILE_SQL"
 		docker exec $ID_Service bash -c "mysql -u root -proot \"appbuilder-admin\" < ./sql/${Module}_$FILE_SQL"
 		docker exec $ID_Service bash -c "rm ./sql/${Module}_$FILE_SQL"
